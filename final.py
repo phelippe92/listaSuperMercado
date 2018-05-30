ea91579f8bab83b878205b366fff47afc3be98cb
phelippe92 <37694383+phelippe92@users.noreply.github.com>
2018-05-30 12:13:08 -0300

    Update final.py

--- final.py
+++ final.py
@@ -19,12 +19,13 @@
 
 import f
 import os
-import time
+from datetime import datetime
 
 listGeralComida = []
 listGeralBebida = []
 listComida = []
 listBebida = []
+
 # ╔═╦═╗
 # ║ ║ ║
 # ╠═╬═╣
@@ -38,9 +39,10 @@ while True:
     print('╚══════════════════════════════════════════════════════════╝')
     print('* COMIDA')
     f.viewListas(listGeralComida)
+    # print(listGeralComida)
     # f.view(listComida)  #   mostra um valor por linha
     print('* BEBIDA')
-    f.view(listGeralBebida)
+    f.viewListas(listGeralBebida)
     print('____________________________________________________________')
 
     if (len(listGeralComida) or len(listGeralBebida)) == 0:
@@ -56,33 +58,39 @@ while True:
         a = f.acao()
         if a == '1':  #   comida
             print('qual comida')
+            listComida[:] = []
             while True:
                 os.system('clear')
 
-                os.system('clear')
                 f.view(listComida)
                 comida = f.comida()
                 
                 if comida == 'sair':
                     break
                 f.add(comida, listComida)
+            
             os.system('clear')
             x = input('Salvar?\n1. Sim\n2. Nao\n>> ')
             if x == ('1' or 'sim'):
                 nome = input('Nome da lista:\n>> ')
-                listComida.insert(0, nome)
-                f.add(listComida, listGeralComida)
-            else:
-                listComida[:] = []
+
+                now = datetime.now()
+                data = str(now.day)+'/'+str(now.month)+'/'+str(now.year)
+                time = str(now.hour)+':'+str(now.minute)+':'+str(int(now.second))
+                horario = '['+data+' '+time+']'
+                
+                listComida[0:0] = [nome+' '+horario]
+
+                # listComida.insert(0, horario)
+                # listComida.insert(0, nome)
+                f.add(listComida[:], listGeralComida)
 
         elif a == '2':    #   bebida
-            os.system('clear')
-            
             print('qual bebida')
+            listBebida[:] = []
             while True:
                 os.system('clear')
 
-                os.system('clear')
                 f.view(listBebida)
                 bebida = f.bebida()
 
@@ -90,7 +98,22 @@ while True:
                     break
 
                 f.add(bebida, listBebida)
-                f.add(listBebida, listGeralBebida)
+                
+            os.system('clear')
+            x = input('Salvar?\n1. Sim\n2. Nao\n>> ')
+            if x == '1' or x == 'sim' or x == 's':
+                nome = input('Nome da lista:\n>> ')
+                
+                now = datetime.now()
+                data = str(now.day)+'/'+str(now.month)+'/'+str(now.year)
+                time = str(now.hour)+':'+str(now.minute)+':'+str(int(now.second))
+                horario = '['+data+' '+time+']'
+
+                listBebida[0:0] = [nome+' '+horario]
+
+                # listBebida.insert(0, horario)
+                # listBebida.insert(0, nome)
+                f.add(listBebida[:], listGeralBebida)
 
     # z = input()
     # if z != '':
